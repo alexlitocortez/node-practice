d8abb3f3fa54e0dbfe6bdd16233161e9ff52c08d
Alex Cortez <acim650@gmail.com>
2022-04-02 02:13:11 -0700

    third push

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,57 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
 const notes = require('./notes.js')
 
-const getNotes = notes('my time is now')
+yargs.version('1.1.0')
 
-console.log(getNotes)
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Create list item',
+    handler: function () {
+        console.log('Creating list item')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading note',
+    handler: function () {
+        console.log('Reading note')
+    }
+})
+
+// add, remove, read, list
+
+yargs.parse()
